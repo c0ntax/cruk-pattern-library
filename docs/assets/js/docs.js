bc814ffd74d7fe83509f317d6be49b0e7a5fc7a6
heylookalive <allister.price@gmail.com>
2016-04-11 19:10:42 +0100

    Update the pattern library with typekit for headings (pattern library documentation site only).

--- docs/assets/js/docs.js
+++ docs/assets/js/docs.js
@@ -9,7 +9,7 @@ jQuery(function() {
       var html = $(this).html();
       $(this).html('');
 
-      var full_html = '<!DOCTYPE html><html lang="en"><head><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="' + document.location.origin + '/cruk-pattern-library/assets/scss/docs.css"></head><body class="example">' + html + '</body></html>';
+      var full_html = '<!DOCTYPE html><html lang="en"><head><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="' + document.location.origin + '/cruk-pattern-library/assets/scss/docs.css"><script src="https://use.typekit.net/bgw6lhg.js"></script><script>try{Typekit.load({ async: true });}catch(e){}</script></head><body class="example">' + html + '</body></html>';
       var $iframe = $('<iframe class="cr-pattern-library-code-example" height="100%" />').appendTo($(this));
       //$iframe.src = "javascript:'"+full_html+"'";
       $iframe[0].contentWindow.document.write(full_html);
